ca0ca8ca85876377fc73d269ac1cdd6572f475d2
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-01-20 11:01:41 -0500

    added

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable resource_group_name {
+  type        = string
+  default     = "acr-resource-group"
+}
+
+variable location {
+  type        = string
+  default     = "East US"
+}
+
+variable acr_name {
+  type        = string
+  default     = "canhub2"
+}
